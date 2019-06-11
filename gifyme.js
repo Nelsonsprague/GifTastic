8da95f6dc25f5f6ccb4881078edcc1b9e7964110
Nelson <nelsonsprague84@gmail.com>
2019-06-11 15:26:55 -0600

    buttons on page!

--- gifyme.js
+++ gifyme.js
@@ -1,10 +1,36 @@
-$(".gif").on("click", function(){
-    if( state === "still"){
-        $(this).attr("src",$(this.attr("data-animate"))
-        );
-        $this.attr("data-state", "animate");
-    }else{
-        $(this).attr("src",$(this).attr("data-still"));
-        $(this).attr("data-state","still");
+var topics = ["poker", "ufc", "biking", "climbing"]
+
+function renderButtons(){
+    $("#buttons-view").empty();
+    for (var i=0; i< topics.length; i++){
+        var a = $("<button>");
+    
+        a.text(topics[i]);
+        $("#buttons-view").append(a);
     }
+}
+$("#buttons-view").on("click", "button" ,function(){
+var search=$(this).text();
+var api = "GJZ9WvM1Yzqy48M1T5lInlpdiDDfCOeM";
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" +api+ "&tag=" +search;
+
+console.log(queryURL)
+$.ajax({
+    url:queryURL,
+    method: "GET"
 })
+
+
+})
+
+renderButtons();
+// $(".gif").on("click", function(){
+//     if( state === "still"){
+//         $(this).attr("src",$(this.attr("data-animate"))
+//         );
+//         $this.attr("data-state", "animate");
+//     }else{
+//         $(this).attr("src",$(this).attr("data-still"));
+//         $(this).attr("data-state","still");
+//     }
+// })
