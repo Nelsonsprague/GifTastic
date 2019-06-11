50668bd549efc0a9694de19547ae0ea301ccf7a6
Nelson <nelsonsprague84@gmail.com>
2019-06-11 10:01:55 -0600

    pause code

---
+++ gifyme.js
@@ -0,0 +1,10 @@
+$(".gif").on("click", function(){
+    if( state === "still"){
+        $(this).attr("src",$(this.attr("data-animate"))
+        );
+        $this.attr("data-state", "animate");
+    }else{
+        $(this).attr("src",$(this).attr("data-still"));
+        $(this).attr("data-state","still");
+    }
+})
